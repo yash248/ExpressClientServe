32ad084f407beb6a3cd78e834b2f547cef0eb1d4
yash248 <dabhi.yash.r@gmail.com>
2017-08-30 00:50:39 -0500

    client serve

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var app = express();
 var path = require('path');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('client', path.join(__dirname, 'client'));
+app.set('view engine', 'pug');	
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(path.join(__dirname + '/public')));
+app.use('/client', express.static(path.join(__dirname + '/client/index.html')));
 
 app.use('/', index);
 app.use('/users', users);
